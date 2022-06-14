c0bbfdbc20f0e09d0d67fe97d1d6772edd840084
Felix Peng <felix.peng@zs.com>
2022-06-14 15:59:37 -0500

    Updated scripts, security
    - requires a config.py file with variables 'elicit_user' and 'elicit_pass' declared as the email/username and password used on elicit.org
    - tests for installation of openai API and avoids running keyword extraction if it is not detected

--- search_compiler.py
+++ search_compiler.py
@@ -9,13 +9,22 @@ Created on Tue Jun 14 12:46:14 2022
 import pandas as pd
 import os
 import glob
+try:
+    import openai
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    no_openai = False
+except ModuleNotFoundError:
+    print('OpenAI library not installed')
+    no_openai = True
+    pass
 
-# for each subtopic, load all csv files and join into a dataframe
+# set up paths
 master_path = os.getcwd()
 result_folder = os.path.join(master_path, 'results')
 subtopics = [1, 2, 3]
 
 
+# function to compile all csv files for a subtopic into a single dataframe
 def load_df_from_folder(ind):
     folder = os.path.join(result_folder, 'subtopic' + str(ind))
     files = glob.glob(os.path.join(folder, '*'))
@@ -24,6 +33,7 @@ def load_df_from_folder(ind):
     return compiled_df
 
 
+# create a compiled df for each subtopic
 try:
     all_dfs
     pass
@@ -32,7 +42,7 @@ except NameError:
     for i in subtopics:
         all_dfs.append(load_df_from_folder(i))
 
-# for each subtopic, identify number of duplicates and number unique results
+# for each subtopic, identify number of unique results
 all_dfs_dedup = []
 n_uniques = []
 for i in range(len(all_dfs)):
@@ -40,6 +50,7 @@ for i in range(len(all_dfs)):
     num_records = len(df)
     df_dedup = df.drop_duplicates()
     num_unique = len(df_dedup)
+    print('Subtopic', i+1, 'unique results: ', num_unique)
     all_dfs_dedup.append(df_dedup)
     n_uniques.append(num_unique)
 
@@ -47,3 +58,31 @@ for i in range(len(all_dfs)):
 master_df = pd.concat(all_dfs, ignore_index=True)
 master_df_dedup = master_df.drop_duplicates()
 num_unique_total = len(master_df_dedup)
+
+# put together keyword prompt
+keyword_prompt = 'Extract keywords from this text:\n\n'
+all_titles = master_df_dedup['Paper title']
+# separate list of titles into chunks that can be handled by OpenAI caller
+chunks = [all_titles[i:i + 10] for i in range(0, len(all_titles), 10)]
+
+'''
+OpenAI API calls
+'''
+if no_openai is False:
+    # perform keyword extraction on titles using OpenAI API
+    keywords = []
+    for i in range(len(chunks)):
+        if (i+1) % 10 == 0:
+            print('Processing chunk', i+1, '/', len(chunks))
+        kprompt = keyword_prompt + '. '.join(chunks[i].values)
+        kprompt = kprompt.replace('..', '.')
+        response = openai.Completion.create(
+            model='text-davinci-002',
+            prompt=kprompt,
+            temperature=0.2,
+            top_p=1.0,
+            frequency_penalty=0.8,
+            presence_penalty=0.0)
+        keywords.append(response['choices'][0]['text'].split('\n\n')[1])
+else:
+    pass
--- search_script.py
+++ search_script.py
@@ -5,6 +5,7 @@ Created on Mon Jun 13 15:21:49 2022
 
 @author: Felix
 """
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -14,15 +15,19 @@ import time
 import itertools
 import os
 import glob
+from config import elicit_user, elicit_pass
 
 global subtopic_count, master_path
 master_path = os.getcwd()
 subtopic_count = 0
 
+
 '''define helper functions'''
 
 
+# collate_terms searches for every term in a set and saves results as csv
 def collate_terms(term_list, overwrite=False):
+    # iterate which subtopic number it is on
     global subtopic_count
     subtopic_count += 1
     print('-------------------------------------------------------------')
@@ -31,15 +36,18 @@ def collate_terms(term_list, overwrite=False):
     fcount = 0
     subfolder = 'subtopic' + str(subtopic_count)
     subpath = os.path.join(result_folder, subfolder)
+    # check whether directory already exists for the subtopic
     if os.path.isdir(subpath) is False:
         print('Creating new directory')
         os.mkdir(subpath)
+    # depending on overwrite flag, may delete existing csv files saved in dir
     if (os.path.isdir(subpath)) & (overwrite is True):
         files = glob.glob(os.path.join(subpath, '*'))
         for f in files:
             os.remove(f)
     elif (os.path.isdir(subpath)) & (overwrite is False):
         pass
+    # iterates through each term and checks for existing
     for t in term_list:
         new_name = t.replace(' ', '') + '.csv'
         nterms = len(term_list)
@@ -48,31 +56,36 @@ def collate_terms(term_list, overwrite=False):
             nterms -= 1
             pass
         else:
+            # if no previous file, perform search, show 24 results
+            #   (click show more twice) and then save as csv
             searchbar = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/div/div/div/div/div[1]/div/form/div/div/textarea[1]')
             searchbar.send_keys(Keys.CONTROL + "a")
             searchbar.send_keys(Keys.DELETE)
             searchbar.send_keys(t)
             searchbar.send_keys(Keys.RETURN)
+            # find and click the show more button twice
             for i in range(2):
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                # show_more = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/div/div/div[1]/div/div[3]/button')
-                # show_more.click()
                 show_more = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/main/div/div/div[1]/div/div[3]/button')))
                 show_more.click()
-                # driver.execute_script("arguments[0].click();", show_more)
+            # if sidebar with 'download csv' button hidden, open it
             try:
                 expand_sidebar = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/div/div/div/div/div[1]/button')
                 expand_sidebar.click()
             except:
                 pass
+            # wait before clicking button (helps with stability)
             time.sleep(3)
+            # find and click download button, then wait for 3s and rename it
             csv = driver.find_element(By.XPATH, '//*[@id="__next"]/div/main/div/div/div[2]/div[2]/div[4]/button[2]')
             csv.click()
             new_name = t.replace(' ', '') + '.csv'
             time.sleep(3)
             os.rename(os.path.join(result_folder, orig_name), os.path.join(subpath, new_name))
             fcount += 1
-            print('File ', fcount, '/', nterms, ' successfully downloaded and renamed!')
+            # print a message for every 10 files to avoid overload
+            if fcount % 10 == 0:
+                print('File ', fcount, '/', nterms, ' successfully downloaded and renamed!')
 
 
 '''define search terms'''
@@ -110,6 +123,8 @@ s3_terms = [x + ' ' + y + ' ' + z for x, y, z
 
 '''selenium instance'''
 # start up selenium driver and retrieve elicit.org login page
+# also set up headless mode (no window opens) and maximized window
+# set default download directory to the results folder
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument('--start-maximized')
@@ -117,6 +132,7 @@ result_folder = os.path.join(master_path, 'results')
 prefs = {"download.default_directory": result_folder}
 options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(options=options)
+# by default, wait up to 15s for an element to be visible or interactable
 driver.implicitly_wait(15)
 driver.get('https://elicit.org/login')
 
@@ -127,14 +143,14 @@ login.click()
 email = driver.find_element(By.XPATH,
                             '//*[@id="ui-sign-in-email-input"]')
 email.click()
-email.send_keys('felix.peng@zs.com')
+email.send_keys(elicit_user)
 email.send_keys(Keys.RETURN)
 pwd = driver.find_element(By.XPATH,
                           '//*[@id="ui-sign-in-password-input"]')
-pwd.send_keys('s7an13317')
+pwd.send_keys(elicit_pass)
 pwd.send_keys(Keys.RETURN)
 
-# initial search
+# initial search to get to normal search page interface
 textarea = driver.find_element(By.XPATH,
                                '//*[@id="__next"]/div/main/div/div/form/div[3]/div/textarea[1]')
 textarea.send_keys('test')
@@ -142,9 +158,9 @@ textarea.send_keys(Keys.RETURN)
 
 # perform searches
 orig_name = 'elicit-results.csv'
-collate_terms(s1_terms)
-collate_terms(s2_terms)
-collate_terms(s3_terms)
+collate_terms(s1_terms, overwrite=True)
+collate_terms(s2_terms, overwrite=True)
+collate_terms(s3_terms, overwrite=True)
 
 # close instance
 driver.close()
